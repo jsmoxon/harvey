3ac1e276e1cb6d7ccece07bc35b3188261fba3a9
Jack Moxon <jackmoxon@jacks-air-2.attlocal.net>
2016-05-07 14:40:54 -0700

    setting up agency table

--- urban_farmers/models.py
+++ urban_farmers/models.py
@@ -2,4 +2,13 @@ from __future__ import unicode_literals
 
 from django.db import models
 
-# Create your models here.
+class Agency(models.Model):
+	agency_name = models.CharField(max_length=200, null=True, blank=True)
+	fed_tax_ID = models.IntegerField(null=True, blank=True)
+	address_line_one = models.CharField(max_length=2000, null=True, blank=True)
+	address_line_two = models.CharField(max_length=2000, null=True, blank=True)
+	address_city = models.CharField(max_length=2000, null=True, blank=True)
+	address_state = models.CharField(max_length=2000, null=True, blank=True)
+	address_zip = models.IntegerField(null=True, blank=True)
+	
+
